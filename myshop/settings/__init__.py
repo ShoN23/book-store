4fc1d7078a7c67e266e280324c4934010bc148b9
Shokhrukh Bekmuradov <shoxxxxx98@gmail.com>
2021-12-03 14:48:02 +0900

    settings

--- myshop/settings/__init__.py
+++
@@ -1,3 +0,0 @@
-from .settings import *
-#from .localadev import *
-from .storage import *
--- myshop/settings/storage.py
+++
@@ -1,9 +0,0 @@
-# AWS storage configurations
-import os
-import environ
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-env = environ.Env()
-environ.Env.read_env()
-
