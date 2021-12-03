4309cbc270079f387263f3154b6e5cfa620a4351
Shokhrukh Bekmuradov <shoxxxxx98@gmail.com>
2021-12-03 10:39:22 +0900

    Database updated

--- myshop/settings/settings.py
+++ myshop/settings/settings.py
@@ -89,22 +89,22 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookstore',
-        'USER': 'postgres',
-        'PASSWORD': env('DATA_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT':'5433'
-    }
-}
 # DATABASES = {
 #     'default': {
-#     'ENGINE': 'django.db.backends.sqlite3',
-#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bookstore',
+#         'USER': 'postgres',
+#         'PASSWORD': env('DATA_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT':'5433'
 #     }
 # }
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
