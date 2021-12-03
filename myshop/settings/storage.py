7b36394c468f7b079aa785793c46d7c6a437c66f
Shokhrukh Bekmuradov <shoxxxxx98@gmail.com>
2021-12-03 11:41:51 +0900

    settings

--- myshop/settings/storage.py
+++ myshop/settings/storage.py
@@ -7,32 +7,3 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 env = environ.Env()
 environ.Env.read_env()
 
-#Boto3 config
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
-CART_SESSION_ID = 'cart'
-
-# AWS config
-AWS_S3_ACCESS_KEY_ID=env('AWS_S3_ACCESS_KEY_ID')
-AWS_S3_SECRET_ACCESS_KEY=env('AWS_S3_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-# A path prefix that will ve prepended to all uploads
-AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-
-# # Django Static files directory
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-# # s3 public media settings
-PUBLIC_MEDIA_LOCATION = 'media'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
